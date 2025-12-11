2a3b4b792fc0196126c425d6eac09d9e713360a8
tariqul420 <tariqul@tariqul.dev>
2025-12-11 17:52:47 +0600

    feat: replace SVG icon with Image component for Google sign-in button and add Google logo asset

--- components/auth/sign-in-with-google.tsx
+++ components/auth/sign-in-with-google.tsx
@@ -3,6 +3,7 @@
 import { signIn } from "@/lib/auth-client";
 import logger from "@/lib/logger";
 import { Loader2 } from "lucide-react";
+import Image from "next/image";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { Button } from "../ui/button";
@@ -35,14 +36,7 @@ export default function SignInWithGoogle() {
       {isGoogleLoading ? (
         <Loader2 className="mr-2 h-4 w-4 animate-spin" />
       ) : (
-        <svg
-          fill="currentColor"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 640 640"
-        >
-          <title>Google</title>
-          <path d="M564 325.8C564 467.3 467.1 568 324 568C186.8 568 76 457.2 76 320C76 182.8 186.8 72 324 72C390.8 72 447 96.5 490.3 136.9L422.8 201.8C334.5 116.6 170.3 180.6 170.3 320C170.3 406.5 239.4 476.6 324 476.6C422.2 476.6 459 406.2 464.8 369.7L324 369.7L324 284.4L560.1 284.4C562.4 297.1 564 309.3 564 325.8z" />
-        </svg>
+        <Image src="/assets/google.png" alt="Google" width={20} height={20} />
       )}
       Continue with Google
     </Button>
