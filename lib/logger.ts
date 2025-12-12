aa4a473c1f97090723fd874efabc7f95279db28c
tariqul420 <tariqul@tariqul.dev>
2025-12-12 22:39:16 +0600

    refactor: simplify logger initialization and remove unused client logger interface

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,60 +1,16 @@
-import { env } from "@/lib/env";
-import type { Logger } from "pino";
+import { pino, type Logger } from "pino";
+import { env } from "./env";
 
-type LogMethod = (...args: unknown[]) => void;
+const logger: Logger = pino({
+  ...(env.isDevelopment && {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+      },
+    },
+  }),
+  level: process.env.PINO_LOG_LEVEL || "info",
+});
 
-interface ClientLogger {
-  info: LogMethod;
-  error: LogMethod;
-  warn: LogMethod;
-  debug: LogMethod;
-  trace: LogMethod;
-  fatal: LogMethod;
-}
-
-const createLogger = (): Logger | ClientLogger => {
-  if (typeof window !== "undefined") {
-    const noop: LogMethod = () => {};
-    return {
-      info: noop,
-      error: noop,
-      warn: noop,
-      debug: noop,
-      trace: noop,
-      fatal: noop,
-    };
-  }
-
-  try {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const pino = require("pino");
-
-    const config = {
-      level: env.isProduction ? "info" : "debug",
-      ...(!env.isProduction && {
-        transport: {
-          target: "pino-pretty",
-          options: {
-            colorize: true,
-            ignore: "pid,hostname",
-            translateTime: "SYS:standard",
-          },
-        },
-      }),
-    };
-
-    return pino(config);
-  } catch (error) {
-    console.error("Failed to initialize Pino logger:", error);
-    return {
-      info: console.info.bind(console),
-      error: console.error.bind(console),
-      warn: console.warn.bind(console),
-      debug: console.debug.bind(console),
-      trace: console.trace.bind(console),
-      fatal: console.error.bind(console),
-    };
-  }
-};
-
-export default createLogger();
+export default logger;
